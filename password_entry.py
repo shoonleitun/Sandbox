e02f44b414e2dc663d406deee1de4333136128e1
shoonleitun <45580219+shoonleitun@users.noreply.github.com>
2019-08-04 13:01:06 +0800

    Update password_entry.py

--- password_entry.py
+++ password_entry.py
@@ -10,29 +10,3 @@ def version_1():
         password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
 
     print('*' * len(password))
-
-
-# version_1()
-
-
-def main():
-    """Get and print password using functions."""
-    password = get_password(MINIMUM_LENGTH)
-    print_asterisks(password)
-
-
-def get_password(minimum_length):
-    """Get password, ensuring it meets the minimum_length requirement."""
-    password = input("Enter password of at least {} characters: ".format(minimum_length))
-    while len(password) < minimum_length:
-        print("Password too short")
-        password = input("Enter password of at least {} characters: ".format(minimum_length))
-    return password
-
-
-def print_asterisks(sequence):
-    """Print as many asterisks as there are characters in the passed-in sequence."""
-    print('*' * len(sequence))
-
-
-main()
