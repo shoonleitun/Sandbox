7481428a31ffdef95589c6c5742337a34e1f0e31
Shoon <shoonleitun@my.jcu.edu.au>
2019-08-04 13:19:43 +0800

    Refactor password program to use function

--- password_entry.py
+++ password_entry.py
@@ -1,11 +1,13 @@
 """Shoon Lei Tun"""
-
 MINIMUM_LENGTH = 5
-
-password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
-while len(password) < MINIMUM_LENGTH:
+def main():
     password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+    while len(password) < MINIMUM_LENGTH:
+        password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+    get_password(password)
 
-print('*' * len(password))
 
+def get_password(passwd):
+    print('*' * len(passwd))
 
+main()
